123c6ba10edaa6c4d8dad9e949c224ece4e2506a
Neotrash788 <82312125+Neotrash788@users.noreply.github.com>
2021-08-18 14:23:58 +0100

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -17,13 +17,15 @@ winner = ""
 
 
 def display_board():
-    print(' | '.join(board[0:3]))
-    print('--+---+--')
-    print(' | '.join(board[3:6]))
-    print('--+---+--')
-    print(' | '.join(board[6:9]))
-    print('---------')
-
+    print(f''' |-----|-----|-----|
+ |  {board[0]}  |  {board[1]}  |  {board[2]}  |
+ |_____|_____|_____|
+ |     |     |     |
+ |  {board[3]}  |  {board[4]}  |  {board[5]}  |
+ |_____|_____|_____|
+ |     |     |     |
+ |  {board[6]}  |  {board[7]}  |  {board[8]}  |
+ |_____|_____|_____|''')
 # Changes turn
 
 
@@ -55,39 +57,50 @@ def check_for_winner():
         return True
 
 
+winner_state = []
+
+
 def check_rows():
-    global winner
+    global winner, winner_state
     if board[0:3] in (["X", "X", "X"], ["O", "O", "O"]):
         winner = board[0]
+        winner_state = [0, 1, 2]
         return True
     elif board[3:6] in (["X", "X", "X"], ["O", "O", "O"]):
         winner = board[3]
+        winner_state = [3, 4, 5]
         return True
     elif board[6:9] in (["X", "X", "X"], ["O", "O", "O"]):
         winner = board[6]
+        winner_state = [6, 7, 8]
         return True
 
 
 def check_columns():
-    global winner
-    if board[0] == board[3] == board[6] in (["X", "X", "X"], ["O", "O", "O"]):
+    global winner, winner_state
+    if board[0] == board[3] == board[6] and board[0] != '-':
         winner = board[0]
+        winner_state = [0, 3, 6]
         return True
-    elif board[1] == board[4] == board[7] in (["X", "X", "X"], ["O", "O", "O"]):
+    elif board[1] == board[4] == board[7] and board[1] != '-':
         winner = board[1]
+        winner_state = [1, 4, 7]
         return True
-    elif board[2] == board[5] == board[8] in (["X", "X", "X"], ["O", "O", "O"]):
+    elif board[2] == board[5] == board[8] and board[2] != '-':
         winner = board[2]
+        winner_state = [2, 5, 8]
         return True
 
 
 def check_diagonals():
-    global winner
-    if board[0] == board[4] == board[8] in (["X", "X", "X"], ["O", "O", "O"]):
+    global winner, winner_state
+    if board[0] == board[4] == board[8] and board[0] != '-':
         winner = board[0]
+        winner_state = [0, 4, 8]
         return True
-    elif board[2] == board[4] == board[6] in (["X", "X", "X"], ["O", "O", "O"]):
+    elif board[2] == board[4] == board[6] and board[2] != '-':
         winner = board[2]
+        winner_state = [2, 4, 6]
         return True
 
 # issue
@@ -102,14 +115,24 @@ def check_if_game_over():
         return True
 
 
-def play_game():
-    global run
+def display_winning_state():
+    global board, winner
+    board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    for i in range(len(winner_state)):
+        board[winner_state[i]] = winner
     display_board()
-    handle_turn(int(input(f"\n{turn}'s turn -> "))-1)
+
+
+def play_game():
+    global run, board
+    display_board()
+    handle_turn(int(input(f"{turn}'s turn -> "))-1)
 
     if check_for_winner():
         run = False
         display_board()
+        print(' <----------------->')
+        display_winning_state()
         print(f"{winner} won!")
     else:
         if check_if_game_over():
